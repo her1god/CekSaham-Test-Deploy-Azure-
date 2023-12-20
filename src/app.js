ddb8a6baa6b8d8ef5f425904f0fb9af2cde22525
21343050 <heripanca33@gmail.com>
2023-12-20 23:25:53 +0700

    pertama commit

--- src/app.js
+++ src/app.js
@@ -72,7 +72,7 @@ app.post("/simpan-stok", async (req, res) => {
 
     await newStock.save();
 
-    await sendStockEmail(email, { symbol, name, logo });
+    await kirimStokEmail(email, { symbol, name, logo });
 
     // res.json({ success: true, message: "Stock saved successfully" });
     res.redirect("/hasilSimpan-stok");
@@ -86,7 +86,8 @@ app.get("", async (req, res) => {
   try {
     // heripanca 33 const apiResponse = await axios.get("https://api.goapi.io/stock/idx/companies?api_key=832addab-1601-59ab-7d6e-eac6dd92");
     // herikondk const apiResponse = await axios.get("https://api.goapi.io/stock/idx/companies?api_key=998156aa-c1b3-5140-462b-b89c8d97");
-    const apiResponse = await axios.get("https://api.goapi.io/stock/idx/companies?api_key=52e53797-fe53-5170-1667-fdb1d8f5");
+    // deni const apiResponse = await axios.get("https://api.goapi.io/stock/idx/companies?api_key=52e53797-fe53-5170-1667-fdb1d8f5");
+    const apiResponse = await axios.get("https://api.goapi.io/stock/idx/companies?api_key=526b919f-1054-5386-2e29-2ecd5dc0");
     console.log("API Response:", apiResponse.data);
 
     // Pemeriksaan untuk memastikan bahwa apiResponse.data.data.results tidak undefined
@@ -114,7 +115,7 @@ app.get("", async (req, res) => {
       logo: company.logo,
     }));
 
-    res.render("index", { companies: companyData, totalCount });
+    res.render("index");
   } catch (error) {
     console.error("Terjadi kesalahan saat mengambil data perusahaan:", error);
     res.render("error");
@@ -124,29 +125,29 @@ app.get("", async (req, res) => {
 // ...
 
 app.get("/search", async (req, res) => {
-  const searchTerm = req.query.search;
-  const searchType = req.query.searchType;
+  const kolomCari = req.query.search;
+  const tipeCari = req.query.tipeCari;
 
   let apiUrl;
 
-  if (searchType === "stock") {
+  if (tipeCari === "stock") {
     // Ganti dengan API endpoint untuk informasi naik turun harga saham
-    apiUrl = `https://api.goapi.io/stock/idx/prices?symbols=${searchTerm}&api_key=52e53797-fe53-5170-1667-fdb1d8f5`;
+    apiUrl = `https://api.goapi.io/stock/idx/prices?symbols=${kolomCari}&api_key=526b919f-1054-5386-2e29-2ecd5dc0`;
   } else {
     // Ganti dengan API endpoint untuk informasi perusahaan
-    apiUrl = `https://api.goapi.io/stock/idx/${searchTerm}/profile?api_key=52e53797-fe53-5170-1667-fdb1d8f5`;
+    apiUrl = `https://api.goapi.io/stock/idx/${kolomCari}/profile?api_key=526b919f-1054-5386-2e29-2ecd5dc0`;
   }
 
   try {
     const apiResponse = await axios.get(apiUrl);
-    const searchData = apiResponse.data.data;
+    const dataCari = apiResponse.data.data;
 
-    if (searchType === "stock") {
+    if (tipeCari === "stock") {
       // Render halaman informasi naik turun harga saham
-      res.render("info-stok", { stockInfo: searchData });
+      res.render("info-stok", { stockInfo: dataCari });
     } else {
       // Render halaman detail perusahaan
-      res.render("detail", { companyDetail: searchData });
+      res.render("detail", { companyDetail: dataCari });
     }
   } catch (error) {
     console.error("Terjadi kesalahan saat mengambil data:", error);
@@ -154,20 +155,18 @@ app.get("/search", async (req, res) => {
   }
 });
 
-// ...
-
 //halaman untuk 404
 app.get("*", (req, res) => {
-  res.send("404", {
-    judul: "404",
-    nama: "heri",
-    pesanKesalahan: "Halaman tidak ditemukan",
+  res.render("404", {
+    title: "404",
+    name: "Heri Ramadhan",
+    pesanError: "Halaman tidak ditemukan",
   });
 });
 
-// Function to send an email using nodemailer
-async function sendStockEmail(to, stockInfo) {
-  // Configure nodemailer transporter
+// fungsi nodemailer untuk kirim email
+async function kirimStokEmail(to, stockInfo) {
+  // pengaturan nodemailer transporter
   const transporter = nodemailer.createTransport({
     service: "gmail",
     auth: {
@@ -176,9 +175,9 @@ async function sendStockEmail(to, stockInfo) {
     },
   });
 
-  // Email content
-  const mailOptions = {
-    from: "", // Replace with your Gmail email
+  // Email konten
+  const pengaturanMail = {
+    from: "", // kirim ke emai; tujuan
     to,
     subject: "Stock Information",
     html: `
@@ -189,8 +188,8 @@ async function sendStockEmail(to, stockInfo) {
     `,
   };
 
-  // Send the email
-  await transporter.sendMail(mailOptions);
+  // kirim email
+  await transporter.sendMail(pengaturanMail);
 }
 
 // Jalankan server
